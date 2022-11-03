7326ad70d55fee78bac8f80c88ae4587d8ccec7b
billygr <billygr@users.noreply.github.com>
2022-11-03 11:45:52 +0000

    Changed the output

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "resource_group_name-serviceA" {
-  value = azurerm_resource_group.rg-serviceA.name
-}
-
-output "resource_group_name-serviceB" {
-  value = azurerm_resource_group.rg-serviceB.name
-}
-
 output "public_ip_address-serviceA" {
   value = azurerm_linux_virtual_machine.vm-serviceA.public_ip_address
 }
 
 output "public_ip_address-serviceB" {
   value = azurerm_linux_virtual_machine.vm-serviceB.public_ip_address
+}
+
+output "public_ip_address-jumpbox" {
+  value = azurerm_linux_virtual_machine.vm-jumpbox.public_ip_address
 }
 
 output "tls_private_key" {
