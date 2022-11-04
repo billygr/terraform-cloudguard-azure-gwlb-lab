a133cff2227f77cb2c5efd3ead439f36da38e2de
billygr <billygr@users.noreply.github.com>
2022-11-04 10:18:02 +0000

    Remove unneeded variable, bring it closer

--- variables.tf
+++ variables.tf
@@ -1,11 +1,6 @@
 variable "resource_group_location" {
-  default     = "eastus"
+  default     = "northeurope"
   description = "Location of the resource group."
-}
-
-variable "resource_group_name_prefix" {
-  default     = "rg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
 variable "my-pub-ip" {
