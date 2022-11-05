c5397096ebb7b941633c04a48517bf0bce54122f
billygr <billygr@users.noreply.github.com>
2022-11-05 08:36:54 +0000

    Added to create the key locally

--- ssh.tf
+++ ssh.tf
@@ -3,3 +3,9 @@ resource "tls_private_key" "example_ssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.example_ssh.private_key_pem
+  filename        = "azureuser.pem"
+  file_permission = "0600"
+}
